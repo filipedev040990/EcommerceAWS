aef9461123451f616de05faf5fd76c59274299b7
Filipe Siqueira <filipedev040990@gmail.com>
2023-02-03 06:34:02 -0300

    feat: implements products stack

--- test/ecommerce_aws.test.ts
+++ test/ecommerce_aws.test.ts
@@ -5,13 +5,14 @@
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/ecommerce_aws-stack.ts
 test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new EcommerceAws.EcommerceAwsStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+  expect(1).toBe(1)
+  //   const app = new cdk.App();
+  //     // WHEN
+  //   const stack = new EcommerceAws.EcommerceAwsStack(app, 'MyTestStack');
+  //     // THEN
+  //   const template = Template.fromStack(stack);
 
 //   template.hasResourceProperties('AWS::SQS::Queue', {
 //     VisibilityTimeout: 300
 //   });
-});
+})
