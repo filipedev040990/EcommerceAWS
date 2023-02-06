678a40f58df540e910d0690ce2da9dc51344cec6
Filipe Siqueira <filipedev040990@gmail.com>
2023-02-06 06:44:11 -0300

    feat: add repository in ProductsFetchFunction

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -1,31 +1,41 @@
 import { type APIGatewayProxyEvent, type APIGatewayProxyResult, type Context } from 'aws-lambda'
+import { ProductRepository } from '/opt/nodejs/productsLayer'
+import { DynamoDB } from 'aws-sdk'
+
+const productsDdb = process.env.PRODUCTS_DDB!
+const ddbClient = new DynamoDB.DocumentClient()
+const productRepository = new ProductRepository(ddbClient, productsDdb)
 
 export const handler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
   const method = event.httpMethod
   const resource = event.resource
 
-  const lambdaRequestId = context.awsRequestId
-  const apiRequestId = event.requestContext.requestId
-
-  console.log(`API Gateway RequestId: ${apiRequestId} - LambdaRequestId: ${lambdaRequestId}`)
-
   if (resource === '/products') {
     if (method === 'GET') {
+      const products = await productRepository.getAll()
+
       return {
         statusCode: 200,
-        body: JSON.stringify({ message: 'GET Products - ok' })
+        body: JSON.stringify(products)
       }
     }
   }
 
   if (resource === '/products/{id}') {
     const productId = event.pathParameters?.id as string
+    try {
+      const product = await productRepository.getById(productId)
 
-    console.log(`GET /products/${productId}`)
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `GET /products/${productId}` })
+      return {
+        statusCode: 200,
+        body: JSON.stringify(product)
+      }
+    } catch (error) {
+      console.error(error)
+      return {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      }
     }
   }
 
