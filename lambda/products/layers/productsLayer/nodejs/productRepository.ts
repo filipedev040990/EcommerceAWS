2fcd369a1ac5d4b000efb8c9937a97cddfa5a9f8
Filipe Siqueira <filipedev040990@gmail.com>
2023-02-07 06:34:05 -0300

    refactor: add productUrl field into productRepository

--- lambda/products/layers/productsLayer/nodejs/productRepository.ts
+++ lambda/products/layers/productsLayer/nodejs/productRepository.ts
@@ -7,6 +7,7 @@ export type Product = {
   code: string
   price: number
   model: string
+  productUrl: string
 }
 
 export class ProductRepository {
@@ -64,12 +65,13 @@ export class ProductRepository {
       },
       ConditionExpression: 'attribute_exists(id)',
       ReturnValues: 'UPDATED_NEW',
-      UpdateExpression: 'set productName = :productName, code = :code, price = :price, model = :model',
+      UpdateExpression: 'set productName = :productName, code = :code, price = :price, model = :model, productUrl = :productUrl',
       ExpressionAttributeValues: {
         ':productName': product.productName,
         ':code': product.code,
         ':price': product.price,
-        ':model': product.model
+        ':model': product.model,
+        ':productUrl': product.productUrl
       }
     }).promise()
 
